e3e4e47d5640cba6f13c7fc2fbbc045d359565bc
1770461138 <67567950+1770461138@users.noreply.github.com>
2020-10-28 16:07:18 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config) {
   const instance =new axios.create({
-    baseURL:'http://185.152.210.136:8000/api/z8',
+    baseURL:'http://152.136.185.210:8000/api/w6',
     timeout:5000
   })
   //1.请求拦截器
